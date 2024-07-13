e07e5e6633b1ccfe681b7b9cfe5f00c8c3ab318f
Kimball Goss <Kimballgoss@gmail.com>
2024-07-12 19:36:28 -0600

    commit 2

--- answers.sql
+++ answers.sql
@@ -2,18 +2,19 @@
 --The Wasatch Cupcake Shop is getting ready to send a marketing email announcing a new promotion on Funfetti cupcakes.
 --We need to get all of our customers’ email addresses to Marketing.
 --Select all of the email addresses from the customers table in alphabetical order.
-SELECT customers FROM cupcakes;
+SELECT email FROM customers ORDER BY email;
 
 --Problem 2#
 --Elizabeth Crocker has requested receipts for all of her past cupcake orders.
 --To look up the receipts, we need to know which orders Elizabeth placed.
 --Write a query that lists the order ids for all orders placed by customers with the first name Elizabeth and last name Crocker.
 --Use a SUBQUERY to do this, not a join.
-
+SELECT * FROM orders WHERE customer_id IN (SELECT id IN customers WHERE fname = "Elizabeth" AND lname = "Crocker");
 
 --Problem 3#
 --Employees need to be scheduled for next week based on how many cupcakes have been ordered and not yet processed.
 --Write a query that gets the total number of cupcakes from unprocessed orders.
+SELECT COUNT(*) FROM orders WHERE processed = false;
 
 
 --Problem 4#
@@ -21,6 +22,7 @@ SELECT customers FROM cupcakes;
 --Write a query that shows the name of each cupcake and the sum of cupcakes ordered for that cupcake type 
 --(for both processed and unprocessed orders), sorted in ascending alphabetical order of cupcake name.
 --The report should show all cupcake types, even if they have not been ordered at all.
+
 
 --Problem 5#
 --Marketing now wants to send an email to its top customers, thanking them for their business and offering them a special 
